56b26311ffc0a73954a17f07391fc8ad938dceb2
Louis Jourdan <jourdanlouis21@gmail.com>
2024-12-19 09:49:05 +0100

    création fonctions Avancer et Reculer

--- Commande_radio/Code/L298N/code_test_v1/code_test_v1.ino
+++ Commande_radio/Code/L298N/code_test_v1/code_test_v1.ino
@@ -2,6 +2,20 @@
 #define IN1 3
 #define IN2 4
 #define EN1 2
+int Avancer (){
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  analogWrite(EN1, 255);
+  delay(3500);
+
+}
+int Reculer (){
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  analogWrite(EN1, 255);
+  delay(3500);
+
+}
 
 void setup() {
   Serial.begin(9600);
@@ -13,13 +27,7 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  analogWrite(EN1, 255);
-  delay(3500);
+  Avancer();
   delay(1000);
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  analogWrite(EN1, 255);
-  delay(3500);
+  Reculer();
 }

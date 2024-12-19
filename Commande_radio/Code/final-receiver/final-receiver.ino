de8b562dcfe293d5c7902139be1bd2560c9455ef
Louis Jourdan <jourdanlouis21@gmail.com>
2024-12-19 09:55:37 +0100

    utilisation du vérin sur le fichier receiver

--- Commande_radio/Code/final-receiver/final-receiver.ino
+++ Commande_radio/Code/final-receiver/final-receiver.ino
@@ -5,9 +5,29 @@
 #define pinCSN 10  // On associe la broche "CSN" du NRF24L01 à la sortie digitale D8 de l'arduino
 #define tunnel "PIPE1"
 #define led 9
-#define borneENB 22
-#define borneIN3 25
-#define borneIN4 26 
+//modification 1 blanc sur jaune
+#define IN1 3
+#define IN2 4
+#define EN1 2
+int Avancer (){
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  analogWrite(EN1, 255);
+  delay(3500);
+
+}
+int Reculer (){
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  analogWrite(EN1, 255);
+  delay(3500);
+
+}
+int ArretMoteur (){
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    analogWrite(EN1, 0);
+}
 
 RF24 radio(pinCE, pinCSN);  // Instanciation du NRF24L01
 
@@ -17,29 +37,15 @@ int on = '1';
 void setup() {
   Serial.begin(9600);
   pinMode(led, OUTPUT);
-  pinMode(borneIN3, OUTPUT);
-  pinMode(borneIN4, OUTPUT);
-  pinMode(borneENB, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(EN1, OUTPUT);
 
   Serial.println("Initialisation radio");
   radio.begin();
   radio.setPALevel(RF24_PA_MAX);
   radio.openReadingPipe(0, adresse);
   radio.startListening();
-
-  Serial.println("Debut test moteurs");
-  Serial.println("Marche avant");
-  digitalWrite(borneIN3, LOW);
-  digitalWrite(borneIN4, HIGH);
-  analogWrite(borneENB, 255);
-  delay(3000);
-  Serial.println("Marche arrière");
-  digitalWrite(borneIN3, HIGH);
-  digitalWrite(borneIN4, LOW);
-  analogWrite(borneENB, 255);
-  delay(3000);
-  Serial.println("Fin test moteurs");
-  delay(500);
 }
 
 void loop() {
@@ -52,10 +58,16 @@ void loop() {
     if (message == on) {
       Serial.println("Message correct reçu: 1");
       digitalWrite(led, HIGH);
+      Avancer();
+      delay(1000);
+      Reculer();
+      ArretMoteur();
     }
     else {
       Serial.println("Message incorrect reçu : 0");
       digitalWrite(led, LOW);
+      ArretMoteur();
+
     }
   }
  }
